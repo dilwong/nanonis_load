966609bc4ce73ed76ed769d6481bab11716461bb
dilwong <dilwong@gmail.com>
2022-10-22 23:29:34 -0400

    Minor: Added long_description_content_type = 'text/markdown' to setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 import setuptools
 
-with open('README.md', 'r') as readme_file:
-    long_description = readme_file.read()
-
 if __name__ == "__main__":
+
+    with open('README.md', 'r') as readme_file:
+        long_description = readme_file.read()
 
     with open('requirements.txt', 'r') as f:
         requirements = f.readlines()
         requirements = [line.strip() for line in requirements if line.strip()]
 
     setuptools.setup(name = 'nanonis_load',
-    version = '2.4.1',
+    version = '2.4.2',
     author = 'Dillon Wong',
     author_email = '',
     description = 'Load Nanonis data',
     long_description = long_description,
+    long_description_content_type = 'text/markdown',
     url = 'https://github.com/dilwong/nanonis_load',
     install_requires = requirements,
     packages=['nanonis_load'],
